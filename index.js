8f344f29dd7cb189e427f1da551c5ec3d8823020
nicolagreco <notsecurity@gmail.com>
2014-08-25 12:54:25 -0700

    simplified file structure

--- index.js
+++ index.js
@@ -34,9 +34,57 @@ function _deployB2G (opts, callback) {
   // TODO: Validate the manifest
   // var validate = validateManifest(opts.manifestURL);
 
-  var webappsActor = Q.ninvoke(opts.client, 'getWebapps');
-  var appActor = findappB2G(opts);
+  
+}
+
+function deployB2G () {
+
+  var args = arguments;
+  var opts = {};
+  var callback;
+
+  /* Overloading */
+
+  // deployB2G(manifestURL, zip [, client])
+  if (typeof args[0] == 'string' && (typeof args[1] == 'string')) {
+    opts.manifestURL = args[0];
+    opts.zip = args[1];
+    if (args[2] instanceof FirefoxClient) {
+      opts.client = args[2];
+    }
+  }
+  // deployB2G({zip: zip_path, manifestURL: manifest_path[, client: firefox_client]})
+  else if (typeof args[0] == 'object') {
+    opts = args[0];
+  }
+
+  // deployB2G(..., callback)
+  if (typeof args[args.length-1] == 'function') {
+    callback = args[args.length-1];
+  }
+
+  /* Options */
+
+  // Missing manifest or zip
+  if (!opts.manifestURL || !opts.zip) {
+    throw new Error('No manifest or zip file');
+  }
+
+  // If no client is passed, it must connect through FirefoxClient
+  if (!opts.client) opts.connect = true;
+
+  // UUID of the app
   var appId = uuid.v1();
+
+  /* Promises */
+
+  var simulator = startB2G(opts);
+  var webappsActor = Q.when(simulator, function() {
+    return Q.ninvoke(opts.client, 'getWebapps');
+  });
+  var appActor = Q.when(simulator, function() {
+    return findappB2G(opts);
+  });
 
   function uninstall () {
     return Q.all([webappsActor, appActor]).spread(function(webapps, app) {
@@ -62,50 +110,13 @@ function _deployB2G (opts, callback) {
     return Q();
   }
 
-  return Q()
+  return simulator
     .then(uninstall)
     .then(install)
     .then(launch)
     .then(function() {
       if (callback) callback(null, appId);
       return appId;
-    });
-}
-
-function deployB2G () {
-
-  var args = arguments;
-  var opts = {};
-  var callback;
-
-  // deployB2G(manifestURL, zip [, client])
-  if (typeof args[0] == 'string' && (typeof args[1] == 'string')) {
-    opts.manifestURL = args[0];
-    opts.zip = args[1];
-    if (args[2] instanceof FirefoxClient) {
-      opts.client = args[2];
-    }
-  }
-  // deployB2G({zip: zip_path, manifestURL: manifest_path[, client: firefox_client]})
-  else if (typeof args[0] == 'object') {
-    opts = args[0];
-  }
-
-  // deployB2G(..., callback)
-  if (typeof args[args.length-1] == 'function') {
-    callback = args[args.length-1];
-  }
-
-
-  // Missing manifest or zip
-  if (!opts.manifestURL || !opts.zip) {
-    throw new Error('No manifest or zip file');
-  }
-
-  return startB2G(opts)
-    .then(function(client) {
-      opts.client = opts.client || client;
-      return _deployB2G(opts, callback);
     });
 }
 
