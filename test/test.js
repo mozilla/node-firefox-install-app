f5af2e843cf14b44a283a97db4256242c59411ad
nicolagreco <notsecurity@gmail.com>
2014-09-17 16:37:56 -0700

    readding tests

--- test/test.js
+++ test/test.js
@@ -22,35 +22,35 @@ describe('fxos-deploy/command', function(done) {
   });
 });
 
-// describe('fxos-deploy', function(){
-//   this.timeout(10000);
-//   afterEach(function() {
-//     Ports({b2g:true}, function(err, instances) {
-//       instances.forEach(function(i) {
-//         process.kill(i.pid);
-//       });
-//     });
-//   });
+describe('fxos-deploy', function(){
+  this.timeout(10000);
+  afterEach(function() {
+    Ports({b2g:true}, function(err, instances) {
+      instances.forEach(function(i) {
+        process.kill(i.pid);
+      });
+    });
+  });
 
-//   describe('when no open simulator', function(){
+  describe('when no open simulator', function(){
 
-//     it('should return app id', function(done) {
+    it('should return app id', function(done) {
 
-//       Connect(function(err, sim) {
-//         return Deploy({
-//           manifestURL: './test/sampleapp/manifest.webapp',
-//           zip: './test/sampleapp/build/app.zip',
-//           client: sim.client
-//         })
-//         .then(function(sim) {
-//           sim.should.be.type('string');
-//         })
-//         .then(done)
-//         .fail(done);
-//       }).done()
+      Connect(function(err, sim) {
+        return Deploy({
+          manifestURL: './test/sampleapp/manifest.webapp',
+          zip: './test/sampleapp/build/app.zip',
+          client: sim.client
+        })
+        .then(function(sim) {
+          sim.should.be.type('string');
+        })
+        .then(done)
+        .fail(done);
+      }).done()
 
-//     });
+    });
 
-//   });
+  });
 
-// });
+});
